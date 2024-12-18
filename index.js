bd4ac6a39c6ee4e21a369edc5d28cd8d0a01470d
FaizanJvd <96561345+FaizanJvd@users.noreply.github.com>
2024-12-18 14:37:17 +0500

    remove host

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors'); // Import cors package
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '192.168.0.105';
 // Middleware for parsing JSON bodies
 app.use(bodyParser.json());
 const connectDB = require("./config/db");
@@ -15,6 +14,6 @@ app.use(cors());
 app.use('/api/users', userRoutes);
 
 // Start the server
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
